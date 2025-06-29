c8b6fe1d5f1f047d096cbd71ebea11bfe7131902
parani <paranitharanparani13@gmail.com>
2025-06-29 22:01:33 +0530

    google oauth added

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import About from "./pages/about";
 import Posts from "./pages/posts";
 import Footer from "./pages/footer"
 import Header from "./components/Header";
+import { signIn } from "@/auth"
 
 export default function Home() {
   return (
@@ -18,7 +19,14 @@ export default function Home() {
 
 
 
-
+  <form
+      action={async () => {
+        "use server"
+        await signIn("google")
+      }}
+    >
+      <button type="submit">Signin with Google</button>
+    </form>
 
       
       <Posts />
--- app/pages/posts/index.tsx
+++ app/pages/posts/index.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button"
 const Posts = () => {
     return (
         <div className='p-4 flex flex-col gap-4'>
-            <h1 className='text-4xl font-bold text-center '>Blog List</h1>
+            <h1 className='text-4xl font-bold text-center '>Latest posts</h1>
             <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4'>
                 {blogs.map((post) => (
                     <div key={post.id} className="">
