a2eb1bf388a9ed2fe2511573ebb4cd56fbea94b2
parani <paranitharanparani13@gmail.com>
2025-07-02 18:07:50 +0530

    use CSR in some components

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,8 +1,9 @@
+'use client'
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4 mt-8">
+    <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center">
         <p className="text-sm">
           © { new Date().getFullYear() } Mini Blog. All rights reserved. 
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -15,13 +15,11 @@ export default async function Header() {
     <header className="bg-slate-900 text-white px-6 py-4 shadow-md sticky w-full ">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         {/* Logo / App Name */}
-        <div className="text-xl font-semibold">Mini-Blog</div>
+        <div className="text-xl font-semibold"><Link href="/">Mini-Blog</Link></div>
         {/* Navigation (Optional for Desktop) */}
         <nav className="hidden md:flex gap-6 items-center">
         
-            <Link href="/about" scroll={true} className="hover:text-slate-300 transition">Home</Link>
-       
-         
+          <Link href="/" className="hover:text-slate-300 transition">Home</Link>
           <Link href="/about" className="hover:text-slate-300 transition">About</Link>
           <Link href="/blogs" className="hover:text-slate-300 transition">Blogs</Link>
         </nav>
